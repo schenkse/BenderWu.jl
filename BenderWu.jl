30714ad05ad9348485ccb3cff9b0acd3377615eb
Sebastian Schenk <schenk.seb@gmail.com>
2026-01-28 18:24:55 +0100

    Add iterative method to compute wave function and energy level coefficients

--- BenderWu.jl
+++ BenderWu.jl
@@ -66,3 +66,59 @@ end
     end
     return ε
 end
+
+# Here goes some iterative solution to the same problem
+function initialize_Akl(ν::Int, l::Int, vcoeffs)
+    kmax = max_k(ν, l, vcoeffs)
+    return zeros(kmax+3, l+1)
+end
+initialize_eps(ν::Int, l::Int) = zeros(l+1)
+
+function fill_Akl!(Akl, ε, ν::Int, maxorder::Int, vcoeffs)
+    ω = sqrt(2.0 * vcoeffs[1])
+    # Be careful with indexing here
+    Akl[ν+1, 1] = 1.0
+    ε[1] = ω * (ν + 1/2)
+    for l=0:maxorder
+        kmax = max_k(ν, l, vcoeffs)
+        # Step 1
+        if l > 0
+            for k=kmax:-1:ν+1
+                Akl[k+1, l+1] += (k+2) * (k+1) * Akl[k+2+1, l+1]
+                for n=1:l-1
+                    Akl[k+1, l+1] += 2 * ε[n+1] * Akl[k+1, l-n+1]
+                end
+                for n=1:l
+                    # Check index bounds
+                    if n+1 > length(vcoeffs) || k-n-2 < 0 continue end
+                    #if iszero(vcoeffs[n+1]) continue end
+                    Akl[k+1, l+1] += -2 * vcoeffs[n+1] * Akl[k-n-2+1, l-n+1]
+                end
+                Akl[k+1, l+1] /= 2 * ω * (k - ν)
+            end
+        end
+        # Step 2
+        if l > 0
+            ε[l+1] += -(ν+2) * (ν+1) / 2 * Akl[ν+2+1, l+1]
+            for n=1:l
+                #if iszero(vcoeffs[n+1]) continue end
+                # Check index bounds
+                if n+1 > length(vcoeffs) || ν-n-2 < 0 continue end
+                ε[l+1] += vcoeffs[n+1] * Akl[ν-n-2+1, l-n+1]
+            end
+        end
+        # Step 3
+        for k=ν-1:-1:0
+            Akl[k+1, l+1] += (k+2) * (k+1) * Akl[k+2+1, l+1]
+            for n=1:l
+                Akl[k+1, l+1] += 2 * ε[n+1] * Akl[k+1, l-n+1]
+                #if iszero(vcoeffs[n+1]) continue end
+                # Check index bounds
+                if n+1 > length(vcoeffs) || k-n-2 < 0 continue end
+                Akl[k+1, l+1] += -2 * vcoeffs[n+1] * Akl[k-n-2+1, l-n+1]
+            end
+            Akl[k+1, l+1] /= 2 * ω * (k - ν)
+        end
+    end
+    nothing
+end
